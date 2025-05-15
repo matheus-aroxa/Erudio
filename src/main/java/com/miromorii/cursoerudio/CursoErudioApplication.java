dcf3dcc5b868556893cfd871116d26350219b5f9
Matheus Aroxa <matheusaroxa13@gmail.com>
2025-05-15 15:52:00 -0300

    Estrutura inicial

---
+++ src/main/java/com/miromorii/cursoerudio/CursoErudioApplication.java
@@ -0,0 +1,13 @@
+package com.miromorii.cursoerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoErudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoErudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miromorii/cursoerudio/CursoErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miromorii.cursoerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoErudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
