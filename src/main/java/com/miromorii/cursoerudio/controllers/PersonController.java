279b039de64019d0a294101171d9d0c4d8b777d7
Mat <miromorii@debian.miromorii>
2025-05-20 17:42:45 -0300

    alterações da sessao 11 - aplicação de migrations

--- src/main/java/com/miromorii/cursoerudio/controllers/PersonController.java
+++ src/main/java/com/miromorii/cursoerudio/controllers/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/persons/v1")
 public class PersonController {
 
     @Autowired
