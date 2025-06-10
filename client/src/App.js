49ee6ea9ecb06c4d5be47b14031afaed850eaa07
miromorii <matheusaroxa13@gmail.com>
2025-06-10 12:19:29 -0300

    tela de login

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './global.css';
+import React from 'react';
+import Login from './pages/login';
+
+export function App() {
+
+  return (
+      <Login />
+  );
+}
---
+++ client/src/pages/login/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+import logo from '../../assets/m.jpg';
+import padlock from '../../assets/padlock.jpg';
+
+export default function Login(){
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logo} className="logo" alt="logo" />
+                <form>
+                    <h1>Acess your account</h1>
+                    <input placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button className="button" type="submit">Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="padlock" />
+        </div>
+    )
+}
